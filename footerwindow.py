e950ccd3e02a13f8b254bc6519addfea00a5c0ad
morley-d <103437424+morley-d@users.noreply.github.com>
2023-11-11 23:14:25 +0600

    Update footerwindow.py

--- footerwindow.py
+++ footerwindow.py
@@ -5,10 +5,3 @@ from creational.factorymethod import *
 class Extra(Toplevel):
     def __init__(self):
         super().__init__()
-        self.title('Footer bar')
-        self.geometry('1280x200')
-        self.button = Button(self, text="Drug plugin here", command=self.show_plugins)
-        self.button.pack(expand=True)
-
-    def show_plugins(self):
-        pass
