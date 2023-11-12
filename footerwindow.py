ffdb24dcfe7c9b0fa13c394d0fa950c2ab0c547f
Andrey <21elektro87@gmail.com>
2023-11-12 17:00:21 +0600

    bug fix

--- footerwindow.py
+++ footerwindow.py
@@ -8,13 +8,13 @@ class Extra(Toplevel):
         self.title("Footer bar")
         self.geometry("1280x200")
         self.entry = Entry(self, width=15, textvariable = StringVar())
-        self.entry.pack(expand=True)
+        self.entry.pack()
         self.button = Button(self, text='Drug plugin here', command=self.show_plugins)
         self.button.pack(expand=True)
 
-def show_plugins(self):
-    drop = self.entry.get()
-    effect = Creator().Factory(drop)
-    if effect:
-        l = Label(self, text="You dropped {}".format(effect.type))
-        l.pack()
+    def show_plugins(self):
+        drop = self.entry.get()
+        effect = Creator().Factory(drop)
+        if effect:
+            l = Label(self, text="You dropped {}".format(effect.type))
+            l.pack()
