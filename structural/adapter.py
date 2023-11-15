d6a285963c2549d640d17697a0a33e1780c9380d
San <andreal87@mail.ru>
2023-11-15 16:19:15 +0600

    implementation added

--- structural/adapter.py
+++ structural/adapter.py
@@ -6,7 +6,18 @@ class Audio(metaclass=ABCMeta):
     def audioRecord():
         pass
 
+
 class Midi():
     @abstractmethod
     def midiTrack():
         pass
+
+
+class AudioTrack(Audio):
+    def audioRecord(self):
+        print("Audio is playing...")
+
+
+class MidiTrack(Midi):
+    def midiTrack(self):
+        print("Midi is playing...")
