debdbb64674599e061a5fa6da543636f8f7ad29f
San <andreal87@mail.ru>
2023-11-17 09:42:57 +0600

    realisation iteratorwindow

--- iteratorwindow.py
+++ iteratorwindow.py
@@ -1,11 +1,29 @@
 from tkinter import *
-from tkinter.messagebox import askquestion
-from structural.facade import *
+from behavioral.iterator import *
 
 class Extra(Toplevel):
     def __init__(self):
         super().__init__()
-        self.title('Rendering')
-        self.geometry('200x700')
-        self.button = Button(self, text='Drug plugin here', command=self.start_rendering)
-        self.button.pack(expand=True)
+        self.title('Iterator')
+        self.geometry('640x480')
+        self.samples = Samples(['hat', 'kick', '808'])
+        self.entry = Entry(self)
+        self.entry.pack()
+        self.entry.bind('<KeyPress>', self.parse)
+        self.listbox = Listbox(self)
+        self.listbox.pack()
+    
+
+    def parse(self, e):
+        self.listbox.delete(0, 'end')
+        char = e.widget.get()
+        data = []
+        for item in self.samples:
+            if char in item:
+                data.append(item)
+            if not char and not char.isspace():
+                return
+            
+        self.listbox.delete(0, 'end')
+        for item in data:
+            self.listbox.insert(0, item)
