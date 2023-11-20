dda26030073b3989bebdb81d385e7c7780e5149b
morley-d <103437424+morley-d@users.noreply.github.com>
2023-11-20 23:55:49 +0300

    hypothesis testing

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ from mywindow import Window
 
 if __name__ == "__main__":
     handle = Window()
-    handle2 = Window()
 
     handle.title('DAW666')
     handle.geometry('1280x720')
 
-    handle2.title('DAW')
-    handle2.geometry('1280x720')
-
     assert(handle is handle2)
 
     handle.mainloop()
-    handle2.mainloop()
