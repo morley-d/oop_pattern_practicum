4888e66a9a1f2577b5a0fd36d87f7353f2bad693
morley-d <103437424+morley-d@users.noreply.github.com>
2023-12-26 23:40:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     # создаем окно
     handle = Window()
 
+    # заголовок для окна
     handle.title('DAW666')
+
+    # размеры окна
     handle.geometry('1280x720')
 
     handle.mainloop()
